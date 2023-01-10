5767e44692e278a8d0b8e18dcf0559b90be6bfda
Роман <rom.krishtal98@gmail.com>
2023-01-10 20:50:02 +0100

    submit with input and delete button added (no func)

--- script.js
+++ script.js
@@ -56,15 +56,51 @@ const inputName = document.getElementById('bookName');
 const inputAuthor = document.getElementById('bookAuthor');
 const inputPage = document.getElementById('bookPages');
 
-submitButton.addEventListener('click', function(index) {
-    const bookItem = document.createElement('div');
-    bookItem.setAttribute('id', index);
-    bookItem.setAttribute('key', index);
-    bookItem.appendChild(createBookElement('h1', `Title: ${inputName.value}`));
-    bookItem.appendChild(createBookElement('h1', `Author: ${inputAuthor.value}`));
-    bookItem.appendChild(createBookElement('h1', `Pages: ${inputPage.value}`));
-    bookItem.appendChild(createBookElement('h1', `Read: ${bookStatus.value}`));
-    books.appendChild(bookItem);
-})
+// submitButton.addEventListener('click', function(index) {
+//     const bookItem = document.createElement('div');
+//     bookItem.setAttribute('id', index);
+//     bookItem.setAttribute('key', index);
+//     bookItem.appendChild(createBookElement('h1', `Title: ${inputName.value}`));
+//     bookItem.appendChild(createBookElement('h1', `Author: ${inputAuthor.value}`));
+//     bookItem.appendChild(createBookElement('h1', `Pages: ${inputPage.value}`));
+//     bookItem.appendChild(createBookElement('h1', `Read: ${bookStatus.value}`));
+//     books.appendChild(bookItem);
+// })
 
 // Form submit with input
+const submitInput = document.getElementById('submitInput');
+
+submitInput.addEventListener('click', function(event) {
+    myLibrary.push(
+    {title: inputName.value, 
+    author: inputAuthor.value,
+    pages: inputPage.value,
+    read: bookStatus.value});
+    const bookItem = document.createElement('div');
+    let name = document.createElement('h1');
+    let author = document.createElement('h1');
+    let page = document.createElement('h1');
+    let readStatus = document.createElement('h1');
+    name.textContent = `Title: ${inputName.value}`;
+    author.textContent = `Author: ${inputAuthor.value}`;
+    page.textContent = `Pages ${inputPage.value}`;
+    readStatus.textContent = `Read: ${bookStatus.value}`;
+    bookItem.appendChild(name);
+    bookItem.appendChild(author);
+    bookItem.appendChild(page);
+    bookItem.appendChild(readStatus);
+    bookItem.appendChild(deleteBook);
+    bookItem.setAttribute('data-book', myLibrary.length);
+    books.appendChild(bookItem);
+    console.log(bookItem);
+    event.preventDefault();
+})
+
+// DeleteButton
+const deleteBook = document.createElement('button');
+deleteBook.setAttribute('class', 'delete');
+deleteBook.textContent = 'Delete';
+
+deleteBook.addEventListener('click', function() {
+    
+})
